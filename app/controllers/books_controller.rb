10367f6adcf7d84543395bcfa7a8e5aee294f717
katsu5277 <winta.kj3344@gmail.com>
2021-06-03 13:48:56 +0000

    bookers2-debug finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,13 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @new_book = Book.new
+    
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -21,10 +25,13 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
-
-
-
+  
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -34,16 +41,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
